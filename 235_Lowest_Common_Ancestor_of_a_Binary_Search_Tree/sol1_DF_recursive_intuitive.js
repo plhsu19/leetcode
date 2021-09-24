5a415b4ed3b5bc7862f2f7ff3420137484429be7
PLHsu <54821322+plhsu19@users.noreply.github.com>
2021-09-24 18:16:26 +0200

    add 235 and 236

---
+++ 235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree/sol1_DF_recursive_intuitive.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+ var lowestCommonAncestor = function(root, p, q) {
+    
+    let LCA;
+
+    function findLca(node, low, hi) {
+        // base-condition not likely to happened
+        if (node === null) return false;
+        // real base-condition: when p.val and q.val is not in the range
+        if ((p.val <= low || p.val >= hi) &&
+            (q.val <= low || q.val >= hi)) {
+            return false;
+        }
+        
+        // chech children node
+        const leftResult = findLca(node.left, low, node.val);
+        const rightResult = findLca(node.right, node.val, hi);
+        
+        // post-order check
+        const currentResult = node.val === p.val || node.val === q.val;
+        
+        if (leftResult + rightResult + currentResult > 1) {
+            LCA = node;
+            return true;
+        }
+        
+        return leftResult || rightResult || currentResult;
+    }
+    findLca(root, -Infinity, Infinity);
+    
+    return LCA;
+};
